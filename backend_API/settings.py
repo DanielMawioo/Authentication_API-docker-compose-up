1161e27b6b884b9b4871ad7c2a850eb6571df884
DanielMawioo <mawioodaniel43@gmail.com>
2021-10-08 15:43:58 +0300

    commit

--- backend_API/settings.py
+++ backend_API/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'backend_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'auth',
+        'USER': 'postgres',
+        'PASSWORD': 'admin12345',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
